9c9781bd14f6d77151fa156cd03e7bc03c82810e
César José Uriarte Chavesta <cesar.uriarte@unmsm.edu.pe>
2023-10-16 23:06:01 -0500

    Dando estilos a los datos

--- src/App.js
+++ src/App.js
@@ -13,35 +13,9 @@ function App() {
         <p className="description-pnf">El PNAF contiene la distribución de frecuencias de los diferentes servicios de telecomunicaciones en el Perú, de tal forma que operen en bandas de frecuencias definidas previamente para cada uno de ellos, a fin de asegurar su operatividad y minimizar la probabilidad de interferencias.</p>
         <div className='container-df'>
           <Rango color="#4CC9F0" imagen='faro'
-            datos=
-            {[
-              {
-                inicio: 3000,
-                final: 9000,
-                Sprimario: ['No atribuida']
-              },
-              {
-                inicio: 9000,
-                final: 14000,
-                Sprimario: ["Radionavegación"]
-              },
-              {
-                inicio: 14000,
-                final: 19950,
-                Sprimario:['Fijo', 'Móvil marítimo']
-              },
-              {
-                inicio: 19950,
-                final: 20050,
-                Sprimario: ["Frecuencias Patrón y Señales Horarias (20 kHz)"]
-              },
-              {
-                inicio: 20050,
-                final: 70000,
-                Sprimario: ['Fijo', 'Móvil marítimo']
-              }
-            ]} />
-          <Rango color="#4CC9F0" imagen='faro'
+            data = {
+              ["VLF (3 kHz - 30 kHz)", 'Very Low Frequency', 'Frecuencia muy baja']
+            }
             datos=
             {[
               {
--- src/components/Rango.jsx
+++ src/components/Rango.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import '../style-sheets/Rango.css'
 
 function Rango(props) {
+
+    const colors = ['#F9F9F9', '#E1F6FF', '#FFC300', '#E2FFE3', '#DAF7A6', '#FF5733'];
+    const colors2 = ['#FFE2E2', '#FFF9E2', '#900C3F', '#581845'];
+
     return (
         <div className="accordion_item" >
             <div className="accordion_header">
@@ -10,19 +14,19 @@ function Rango(props) {
                 </div>
                 <div className="text_header">
                     <div className="text_header-container">
-                        <div className='frecuencia'>VLF (3 kHz - 30 kHz)</div>
-                        <div className='name-frecuencia-english' >Very Low Frequency</div>
-                        <div className='name-frecuencia-espanish'>Frecuencia muy baja</div>
+                        <div className='frecuencia'>{props.data[0]}</div>
+                        <div className='name-frecuencia-english' >{props.data[1]}</div>
+                        <div className='name-frecuencia-espanish'>{props.data[2]}</div>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className='propiedades-container'>
                 {
                     props.datos.map((propiedad, index) => (
-                        <div key={index} className='primary'>
+                        <div key={index} className='primary' style={{height: `${Math.log10(propiedad.final - propiedad.inicio) * 15}px`, backgroundColor: `${colors[index]}`}}>
                             {propiedad.Sprimario.length >= 2 ? (
                                 propiedad.Sprimario.map((valor, subindex) => (
-                                    <div key={subindex} className='primaryS'>{valor}</div>
+                                    <div key={subindex} className='primaryS' style={{ width: `calc(100% / ${propiedad.Sprimario.length})`, backgroundColor: `${colors2[subindex]}`}}>{valor}</div>
                                 ))
                             ) : (
                                 propiedad.Sprimario[0]
