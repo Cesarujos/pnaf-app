fd9b39daeffbe8b821578116277d6cc5c18cb064
Cesarujos <88691959+Cesarujos@users.noreply.github.com>
2023-10-17 16:20:18 +0000

    Funcionando pero falta que al abrir se cierre los demas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,97 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import Rango from './components/Rango';
 
+const rangoVLF= {
+  color: "#4CC9F0",
+  imagen: "faro",
+  title: ["VLF (3 kHz - 30 kHz)", 'Very Low Frequency', 'Frecuencia muy baja'],
+  propiedades: [
+    {
+      inicio: 3000,
+      final: 9000,
+      Sprimario: ['No atribuida']
+    },
+    {
+      inicio: 9000,
+      final: 14000,
+      Sprimario: ["Radionavegación"]
+    },
+    {
+      inicio: 14000,
+      final: 19950,
+      Sprimario: ['Fijo', 'Móvil marítimo']
+    },
+    {
+      inicio: 19950,
+      final: 20050,
+      Sprimario: ["Frecuencias Patrón y Señales Horarias (20 kHz)"]
+    },
+    {
+      inicio: 20050,
+      final: 70000,
+      Sprimario: ['Fijo', 'Móvil marítimo']
+    }
+  ]
+}
+const rangoLF = {
+  color: "#4895EF",
+  imagen: "shipcontainer",
+  title: ["LF (30 kHz - 300 kHz)", "Low Frequency", "Frecuencia baja"],
+  propiedades: [
+    {
+        inicio: 20050,
+        final: 70000,
+        Sprimario: ["Fijo", "Móvil Marítimo"]
+    },
+    {
+        inicio: 70000,
+        final: 90000,
+        Sprimario: ["Fijo", "Móvil Marítimo", "Radionavegación Marítima", "Radiolocalización"]
+    },
+    {
+        inicio: 90000,
+        final: 110000,
+        Sprimario: ["Radionavegación", "Fijo"]
+    },
+    {
+        inicio: 110000,
+        final: 130000,
+        Sprimario: ["Fijo","Móvil Marítimo","Radionavegación Marítima","Radiolocalización"]
+    },
+    {
+        inicio: 130000,
+        final: 160000,
+        Sprimario: ["Fijo","Móvil Marítimo"]
+    },
+    {
+        inicio: 160000,
+        final: 190000,
+        Sprimario: ["Fijo"]
+    },
+    {
+        inicio: 190000,
+        final: 200000,
+        Sprimario: ["Radionavegación Aeronáutica"]
+    },
+    {
+        inicio: 200000,
+        final: 275000,
+        Sprimario: ["Radionavegación Aeronáutica"]
+    },
+    {
+        inicio: 275000,
+        final: 285000,
+        Sprimario: ["Radionavegación Aeronáutica","Móvil aeronáutico","Radionavegación marítima (radiofaros)"]
+    },
+    {
+        inicio: 285000,
+        final: 315000,
+        Sprimario: ["Radionavegación marítima (radiofaros)","Radionavegación aeronáutica"]
+    }
+  ]
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -12,38 +103,12 @@ function App() {
         <h1 className="tiltle-pnf">Plan Nacional de Atribución de Frecuencias</h1>
         <p className="description-pnf">El PNAF contiene la distribución de frecuencias de los diferentes servicios de telecomunicaciones en el Perú, de tal forma que operen en bandas de frecuencias definidas previamente para cada uno de ellos, a fin de asegurar su operatividad y minimizar la probabilidad de interferencias.</p>
         <div className='container-df'>
-          <Rango color="#4CC9F0" imagen='faro'
-            data = {
-              ["VLF (3 kHz - 30 kHz)", 'Very Low Frequency', 'Frecuencia muy baja']
-            }
-            datos=
-            {[
-              {
-                inicio: 3000,
-                final: 9000,
-                Sprimario: ['No atribuida']
-              },
-              {
-                inicio: 9000,
-                final: 14000,
-                Sprimario: ["Radionavegación"]
-              },
-              {
-                inicio: 14000,
-                final: 19950,
-                Sprimario:['Fijo', 'Móvil marítimo']
-              },
-              {
-                inicio: 19950,
-                final: 20050,
-                Sprimario: ["Frecuencias Patrón y Señales Horarias (20 kHz)"]
-              },
-              {
-                inicio: 20050,
-                final: 70000,
-                Sprimario: ['Fijo', 'Móvil marítimo']
-              }
-            ]} />
+          <Rango color={rangoVLF.color} imagen={rangoVLF.imagen}
+            data={rangoVLF.title}
+            datos={rangoVLF.propiedades} />
+          <Rango color={rangoLF.color} imagen={rangoLF.imagen}
+            data={rangoLF.title}
+            datos={rangoLF.propiedades} />
         </div>
       </div>
     </div>
--- src/components/Rango.jsx
+++ src/components/Rango.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style-sheets/Rango.css'
 
 function Rango(props) {
 
     const colors = ['#F9F9F9', '#E1F6FF', '#FFC300', '#E2FFE3', '#DAF7A6', '#FF5733'];
-    const colors2 = ['#FFE2E2', '#FFF9E2', '#900C3F', '#581845'];
+    const colors2 = ['#FFE2E2', '#FFF9E2', '#FFF9E2', '#FE2E2','#9FF9E2'];
 
+    const [isClick, setIsClick] = useState(false);
+
+    function handleAccordionClick() {
+        setIsClick(!isClick);
+    }
     return (
-        <div className="accordion_item" >
-            <div className="accordion_header">
-                <div className="image_header-container" style={{ backgroundColor: props.color }}>
-                    <img className='imagen-header' src={require(`../IconsComponent/${props.imagen}.svg`)} alt="icono" />
+        <div className={`accordion_item ${isClick ? 'accordion_item_2' : ''}`} >
+            <div className={`accordion_header ${isClick ? 'accordion_header_2' : ''}`}>
+                <div className="image_header-container" style={{ backgroundColor: props.color }} onClick={handleAccordionClick}>
+                    <img className= {`imagen-header ${isClick ? 'imagen-header_2' : ''}`} src={require(`../IconsComponent/${props.imagen}.svg`)} alt="icono" />
                 </div>
-                <div className="text_header">
+                <div className={`text_header ${isClick ? 'text_header_2' : ''}`}>
                     <div className="text_header-container">
                         <div className='frecuencia'>{props.data[0]}</div>
                         <div className='name-frecuencia-english' >{props.data[1]}</div>
@@ -20,10 +25,10 @@ function Rango(props) {
                     </div>
                 </div>
             </div>
-            <div className='propiedades-container'>
+            <div className= {`propiedades-container ${isClick ? 'propiedades-container_2' : ''}`}>
                 {
                     props.datos.map((propiedad, index) => (
-                        <div key={index} className='primary' style={{height: `${Math.log10(propiedad.final - propiedad.inicio) * 15}px`, backgroundColor: `${colors[index]}`}}>
+                        <div key={index} className='primary' style={{height: `${Math.log10(propiedad.final - propiedad.inicio) * 15 + 10}px`, backgroundColor: `${colors[index]}`}}>
                             {propiedad.Sprimario.length >= 2 ? (
                                 propiedad.Sprimario.map((valor, subindex) => (
                                     <div key={subindex} className='primaryS' style={{ width: `calc(100% / ${propiedad.Sprimario.length})`, backgroundColor: `${colors2[subindex]}`}}>{valor}</div>
